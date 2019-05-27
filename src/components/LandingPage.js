0e2dcb02fcf3e8b43a311ecf5b2022387ea14a85
IshaniDolawatta <ishanihans@gmail.com>
2019-05-27 11:54:07 +0530

    [Feature] theme switcher (#3)
    * import images and include in main web view
    
    * Implemented mulitiple timers
    
    * Minor fixes
    
    * light and dark themes switch according to the day time
    
    * minor changes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,26 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Countdown from "./timer/countdown/Countdown";
 
-class LandingPage extends Component {
+import playIconDark from "../assets/icons/play-icon-dark.svg";
+import playIconLight from "../assets/icons/play-icon-light.svg";
+import googlePlay from "../assets/images/google-play.svg";
+import appStore from "../assets/images/app-store.svg";
 
-    state = {
-      timer: true,
-    };
+import "./LandingPage.scss";
+
+class LandingPage extends Component {
+  state = {
+    currentTime: new Date().getHours(),
+    firstTimer: true,
+    secondTimer: false,
+    thirdTimer: false
+  };
+
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  startTimer = () => {
+    this.timer = setInterval(() => {
+      this.setState({
+        currentTime: new Date().getHours()
+      });
+    }, 1000);
+  };
+
+  endFirstTimer = () => {
+    this.setState({ firstTimer: false, secondTimer: true });
+  };
+
+  endSecondTimer = () => {
+    this.setState({ secondTimer: false, thirdTimer: true });
+  };
+
+  endThirdTimer = () => {
+    this.setState({ thirdTimer: false });
+  };
 
   render() {
-    const { timer} = this.state;
+    const { firstTimer, secondTimer, thirdTimer, currentTime } = this.state;
+    const isDark = currentTime < 18;
     return (
-      <div className="App">
+      <div
+        className={`main-container ${
+          isDark ? "main-container--light" : "main-container--dark"
+        }`}
+      >
         <div className="row">
           <div className="col-md-6">
-            <p className="discription"></p>
+            <div className="description-container">
+              <div>
+                <p className="description-container__main-description mb-0">
+                  Blackbird TM Timer is a simplistic timer for all of you doing
+                  Trancendental Meditation.
+                </p>
+                <p className="description-container__main-description">
+                  With creative energy from learning TM in Rishikesh in 1968
+                  Paul McCartney composed the simple and beautiful song
+                  Blackbird.
+                </p>
+
+                <div className="mt-4 ml-2">
+                  <img src={isDark ? playIconLight : playIconDark} />
+                </div>
+                <p className="description-container__main-description mt-3">
+                  Please try the timer here or download it on Appstore and
+                  Google play.
+                </p>
+              </div>
+              <div className="description-container__app-links mt-5 ">
+                <img src={googlePlay} />
+                <img src={appStore} />
+              </div>
+            </div>
           </div>
           <div className="col-md-6">
-            <div className="Timers">{ timer &&
-              <div>         
-                <p>1</p>
-                <Countdown duration={30000}/>
-              </div>  }
+            <div className="timers">
+              {firstTimer && (
+                <Countdown duration={30000} endTimer={this.endFirstTimer} />
+              )}
+              {secondTimer && (
+                <Countdown duration={40000} endTimer={this.endSecondTimer} />
+              )}
+              {thirdTimer && (
+                <Countdown duration={50000} endTimer={this.endThirdTimer} />
+              )}
             </div>
           </div>
         </div>
