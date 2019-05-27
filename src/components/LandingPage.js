f247b92319c359025214b5343ab4c88b549effe9
Kithma Marindagoda <kmarindagoda@gmail.com>
2019-05-27 10:03:04 +0530

    import images and include in main web view

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,26 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Countdown from "./timer/countdown/Countdown";
 
-class LandingPage extends Component {
+import playIconDark from "../assets/icons/play-icon-dark.svg";
+import googlePlay from "../assets/images/google-play.svg";
+import appStore from "../assets/images/app-store.svg";
 
-    state = {
-      timer: true,
-    };
+import "./LandingPage.scss";
+
+class LandingPage extends Component {
+  state = {
+    timer: true
+  };
 
   render() {
-    const { timer} = this.state;
+    const { timer } = this.state;
     return (
-      <div className="App">
+      <div className="background-color-day">
         <div className="row">
           <div className="col-md-6">
-            <p className="discription"></p>
+            <div className="description-container">
+              <div>
+                <p className="description-container__main-description mb-0">
+                  Blackbird TM Timer is a simplistic timer for all of you doing
+                  Trancendental Meditation.
+                </p>
+                <p className="description-container__main-description">
+                  With creative energy from learning TM in Rishikesh in 1968
+                  Paul McCartney composed the simple and beautiful song
+                  Blackbird.
+                </p>
+
+                <div className="mt-4 ml-2">
+                  <img src={playIconDark} />
+                </div>
+                <p className="description-container__main-description mt-3">
+                  Please try the timer here or download it on Appstore and
+                  Google play.
+                </p>
+              </div>
+              <div className="description-container__app-links mt-5 ">
+                <img src={googlePlay} />
+                <img src={appStore} />
+              </div>
+            </div>
           </div>
           <div className="col-md-6">
-            <div className="Timers">{ timer &&
-              <div>         
-                <p>1</p>
-                <Countdown duration={30000}/>
-              </div>  }
+            <div className="timers">
+              {timer && (
+                <div>
+                  <p>1</p>
+                  <Countdown duration={30000} />
+                </div>
+              )}
             </div>
           </div>
         </div>
