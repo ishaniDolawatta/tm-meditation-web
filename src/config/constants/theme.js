1ed83327c428e28850cec1f8c0c3a5a44ba29707
“IshaniDolawatta” <ishanihans@gmail.com>
2019-05-28 12:53:28 +0530

    Minor fix

--- src/config/constants/theme.js
+++ src/config/constants/theme.js
@@ -3,4 +3,4 @@ import moment from "moment";
 export const THEME_TYPE_DAY = "THEME_TYPE_DAY"; 
 export const THEME_TYPE_NIGHT = "THEME_TYPE_NIGHT"; 
 export const DAY_TIME = moment({ hour: 6 });
-export const NIGHT_TIME = moment({ hour: 18 });;
+export const NIGHT_TIME = moment({ hour: 18 });
