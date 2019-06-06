f20b192938d1c7587e381f55a6ef38f36dc48a49
dhanukaperera <dhanukashehanperera@gmail.com>
2019-06-06 13:25:02 +0530

    fix text space

--- src/config/constants/description.js
+++ src/config/constants/description.js
@@ -1,6 +1,6 @@
 export const FIRST_DESCRIPTION =
   "Blackbird TM Timer is a simplistic timer for all of you doing Trancendental Meditation.";
 export const SECOND_DESCRIPTION =
-  "With creative energy from learning TM in Rishikesh in 1968 Paul McCartney composed the simple and beautiful songBlackbird.";
+  "With creative energy from learning TM in Rishikesh in 1968 Paul McCartney composed the simple and beautiful song Blackbird.";
 export const APP_LINKS_DESCRIPTION =
   "Please try the timer here or download it on Appstore and Google play.";
